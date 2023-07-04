c46eebd58fdee334cbf3398eece0a2bc4f000bea
“Nickhil <suryanscrit@gmail.com>
2023-07-04 13:45:15 -0500

    "no changes"

--- blocks.rb
+++ blocks.rb
@@ -9,7 +9,11 @@ loop do
   blocks << {
     height: height,
     # We're now using the last block's hash instead of its height.
-    last_block: height -1,
+    last_block: blocks.last&.hash,
     transactions: transactions_for_this_block,
   }
+
+  block ["hash"] = Digest::SHA256.hexdigest(block.to_json)
+  
+  blocks << block
 end
